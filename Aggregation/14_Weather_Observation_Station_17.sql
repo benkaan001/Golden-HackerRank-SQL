2548b9b65050e98633566c62e841ef52e137ce85
benkaan001 <benkaan001@gmail.com>
2022-12-09 00:12:45 -0600

    add aggregation solution 14

---
+++ Aggregation/14_Weather_Observation_Station_17.sql
@@ -0,0 +1,24 @@
+/*
+Query the Western Longitude (LONG_W)
+where the smallest Northern Latitude (LAT_N) in STATION is greater than 38.7780.
+Round your answer to 4 decimal places.
+
+
+STATION TABLE:
+
+Field       Type
+ID          NUMBER
+CITY        VARCHAR(21)
+STATE       VARCHAR(2)
+LAT_N       NUMBER      -- Northern Latitude
+LONG_W      NUMBER      -- Western Longtitude
+
+*/
+
+SELECT ROUND(LONG_W, 4)
+FROM station
+WHERE LAT_N = (
+    SELECT MIN(LAT_N)
+    FROM station
+    WHERE LAT_N > 38.7780
+    );
