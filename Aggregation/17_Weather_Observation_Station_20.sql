8f1154f93c54054846416160f5f15b6477b779af
benkaan001 <benkaan001@gmail.com>
2022-12-09 22:03:42 -0600

    update aggregation solution 17

--- Aggregation/17_Weather_Observation_Station_20.sql
+++ Aggregation/17_Weather_Observation_Station_20.sql
@@ -15,3 +15,24 @@ LAT_N       NUMBER      -- Northern Latitude
 LONG_W      NUMBER      -- Western Longtitude
 
 */
+
+SET @rownum := - 1;
+
+SELECT ROUND(LAT_N, 4)
+FROM (
+    SELECT LAT_N, (@rownum := @rownum + 1) AS rownum
+    FROM station
+    ORDER BY LAT_N
+) AS sub
+WHERE sub.rownum IN ( FLOOR(@rownum / 2), CEIL(@rownum / 2));
+
+
+
+SELECT COUNT(*)
+FROM station;    /* Find out the total count of records, which returns 449*/
+
+SELECT ROUND(LAT_N, 4)
+FROM station
+ORDER BY LAT_N
+LIMIT 1 OFFSET 249; /* Return the median record which is (449-1)/2 */
+
