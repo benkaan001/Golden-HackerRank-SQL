eb81988349bdf08cf5396fdd2e21cc00e3d86d67
benkaan001 <benkaan001@gmail.com>
2022-12-09 21:54:20 -0600

    update aggregation solution 17

---
+++ Aggregation/17_Weather_Observation_Station_20.sql
@@ -0,0 +1,17 @@
+/*
+
+A median is defined as a number separating the higher half of a data set from the lower half.
+Query the median of the Northern Latitudes (LAT_N) from STATION
+and round your answer to 4 decimal places.
+
+
+STATION TABLE:
+
+Field       Type
+ID          NUMBER
+CITY        VARCHAR(21)
+STATE       VARCHAR(2)
+LAT_N       NUMBER      -- Northern Latitude
+LONG_W      NUMBER      -- Western Longtitude
+
+*/
